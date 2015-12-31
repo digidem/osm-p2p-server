523fdb177b60ba496cf43dcac51fcb566ff5e6af
James Halliday <mail@substack.net>
2015-12-31 01:14:30 -0500

    hypothetical usage

---
+++ example/server.js
@@ -0,0 +1,29 @@
+var level = require('level')
+var hyperlog = require('hyperlog')
+var fdstore = require('fd-chunk-store')
+var db = {
+  log: level('/tmp/osm-p2p/log'),
+  index: level('/tmp/osm-p2p/index')
+}
+
+var osmdb = require('osm-p2p-db')
+var osm = osmdb({
+  log: hyperlog(db.log, { valueEncoding: 'json' }),
+  db: db.index,
+  store: fdstore(4096, '/tmp/osm-p2p/tree'),
+  size: 4096
+})
+
+var osmrouter = require('../')
+var router = osmrouter(osm)
+
+var http = require('http')
+var server = http.createServer(function (req, res) {
+  var m = router.match(req.method, req.url)
+  if (m) m.fn(req, res)
+  else {
+    res.statusCode = 404
+    res.end('not found\n')
+  }
+})
+server.listen(5000)
