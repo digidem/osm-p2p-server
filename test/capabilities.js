a8fd7aa16b8cc50ab29538084cc5979f5579105a
James Halliday <mail@substack.net>
2016-02-05 16:34:33 -0800

    capabilities test

---
+++ test/capabilities.js
@@ -0,0 +1,42 @@
+var test = require('tape')
+var request = require('http').request
+var tmpdir = require('os').tmpdir()
+var path = require('path')
+var osmrouter = require('../')
+var http = require('http')
+var osmdb = require('osm-p2p')
+var parsexml = require('nodexml').xml2obj
+var hyperquest = require('hyperquest')
+var concat = require('concat-stream')
+
+var port, server
+test('setup capabilities', function (t) {
+  var osm = osmdb(path.join(tmpdir, 'osm-p2p-server-test-' + Math.random()))
+  var router = osmrouter(osm)
+
+  server = http.createServer(function (req, res) {
+    if (router.handle(req, res)) {}
+    else {
+      res.statusCode = 404
+      res.end('not found\n')
+    }
+  })
+  server.listen(0, function () {
+    port = server.address().port
+    t.end()
+  })
+})
+
+test('capabilities', function (t) {
+  t.plan(1)
+  hyperquest('http://localhost:' + port + '/api/capabilities')
+    .pipe(concat(function (body) {
+      var data = parsexml(body)
+      t.equal(data.osm.version, '0.6')
+    }))
+})
+
+test('teardown capabilities', function (t) {
+  server.close()
+  t.end()
+})
