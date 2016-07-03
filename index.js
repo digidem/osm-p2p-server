3b60d11c0baadfdd8c99d3b15e1ea8244b61b21f
James Halliday <mail@substack.net>
2016-07-02 19:40:16 -0700

    refactor routes to include method in pattern

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-var Router = require('routes')
-var routes = require('./lib/routes.js')
+var router = require('./lib/routes.js')
 
 module.exports = Server
 
@@ -7,26 +6,15 @@ function Server (osmdb) {
   if (!(this instanceof Server)) return new Server(osmdb)
   var self = this
   self.osmdb = osmdb
-  self.routers = {
-    get: Router(),
-    post: Router(),
-    put: Router(),
-    delete: Router()
-  }
-  routes.forEach(function (r) {
-    self.routers[r[0]].addRoute(r[1], r[2])
-  })
+  self.router = router
 }
 
 Server.prototype.match = function (method, url) {
-  method = method.toLowerCase()
-  if (!this.routers.hasOwnProperty(method)) return null
-  return this.routers[method].match(url)
+  return this.router.match(method.toUpperCase() + ' ' + url)
 }
 
 Server.prototype.handle = function (req, res) {
   var method = (req.headers.X_HTTP_METHOD_OVERRIDE || req.method)
-    .toLowerCase()
   var m = this.match(method, req.url)
   if (m) {
     m.fn(req, res, this.osmdb, m)
