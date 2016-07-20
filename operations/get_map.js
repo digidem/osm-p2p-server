6dd226b7601185986922a579a4c591c000c2d899
Gregor MacLennan <gmaclennan@digital-democracy.org>
2016-07-19 21:05:06 -0700

    pumpify pipeline = better error handling and closing of streams

--- operations/get_map.js
+++ operations/get_map.js
@@ -1,9 +1,11 @@
+var pumpify = require('pumpify')
+
 var toOsmObj = require('../transforms/osm_p2p_to_obj.js')
 var checkRefExist = require('../lib/check_ref_ex.js')
 
 module.exports = function getMap (bbox, osm, opts) {
   var query = [[bbox[1], bbox[3]], [bbox[0], bbox[2]]] // left,bottom,right,top
   var queryStream = osm.queryStream(query)
-  var queryStreamJson = queryStream.pipe(checkRefExist(osm)).pipe(toOsmObj())
+  var queryStreamJson = pumpify.obj(queryStream, checkRefExist(osm), toOsmObj())
   return queryStreamJson
 }
