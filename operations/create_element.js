6acdce40698df78aab850218022574d21760d5db
Gregor MacLennan <gmaclennan@digital-democracy.org>
2016-07-21 11:13:49 -0700

    Only create elements when a changeset exists and is open.

--- operations/create_element.js
+++ operations/create_element.js
@@ -6,14 +6,31 @@ var hex2dec = require('../lib/hex2dec.js')
 
 module.exports = function createElement (element, osm, cb) {
   var id = hex2dec(randombytes(8).toString('hex'))
+  var changesetId = element.changeset
 
-  if (/^(node|way|relation)$/.test(element.type) && !element.changeset) {
-    return cb(createError(400, 'missing changeset'))
+  if (!changesetId) {
+    return cb(createError(400, 'missing changeset ID: the element must have' +
+      'a changeset attribute with the id of an open changset'))
   }
 
-  element.timestamp = element.timestamp || new Date().toISOString()
-  osm.put(id, obj2P2p.fn(element), function (err, node) {
+  osm.get(changesetId, function (err, docs) {
     if (err) return cb(err)
-    cb(null, id, node)
+    if (Object.keys(docs).length === 0) {
+      return cb(createError(400, 'changeset ' + changesetId + 'not found.\n' +
+        'The element must reference an existing changeset.'))
+    }
+    var closedAt = Object.keys(docs).reduce(function (p, v) {
+      if (p) return p
+      return docs[v].closedAt || docs[v].closed_at
+    }, false)
+    if (closedAt) {
+      return cb(createError(409, 'The changeset ' + changesetId +
+        ' was closed at ' + closedAt + '.'))
+    }
+    element.timestamp = element.timestamp || new Date().toISOString()
+    osm.put(id, obj2P2p.fn(element), function (err, node) {
+      if (err) return cb(err)
+      cb(null, id, node)
+    })
   })
 }
