7bc314f5b2c9356c63f605dccb6a7bb2ff67ddf9
Gregor MacLennan <gmaclennan@digital-democracy.org>
2016-08-18 12:53:56 -0700

    Breaking: only return forks of elements if forks=true URL param is set.

--- api/get_element.js
+++ api/get_element.js
@@ -7,12 +7,7 @@ module.exports = function (osm) {
   return function getElement (id, cb) {
     osm.get(id, function (err, docs) {
       if (err) return cb(err)
-      // Sorting forks to try to maintain some consistency in the order they
-      // are sent to the client, since most clients will only read the first
-      // element returned by this end-point, and sort order is not guaranteed
-      // to stay the same on the `docs` object
-      // TODO: Sort by date?
-      var forks = Object.keys(docs).sort()
+      var forks = Object.keys(docs)
       if (forks.length === 0) return cb(new errors.NotFound('element id: ' + id))
       forks = forks.map(function (key) {
         return xtend(docs[key], {
--- handlers/element_read.js
+++ handlers/element_read.js
@@ -7,7 +7,18 @@ var objToXml = require('../transforms/obj_to_xml.js')
 module.exports = function (req, res, api, params, next) {
   api.getElement(params.id, function (err, forks) {
     if (err) return next(err)
+    if (!params.forks) {
+      forks = forks.sort(recentFirst)[0]
+    }
     res.setHeader('content-type', 'text/xml; charset=utf-8')
     pump(fromArray.obj(forks), objToXml(), wrapResponse(), res, next)
   })
+}
+
+function recentFirst (a, b) {
+  if (a.timestamp && b.timestamp) {
+    return b.timestamp - a.timestamp
+  }
+  // Ensure sorting is consistent between requests
+  return a.version < b.version ? -1 : 1
 }
