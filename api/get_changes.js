954f2725e8d31e268e8c6632c4b6ef9450cfac57
Gregor MacLennan <gmaclennan@digital-democracy.org>
2016-08-18 21:11:01 -0700

    Move getChanges logic from osm-p2p-db into here
    osm.getChanges from osm-p2p-db returns a list of version ids, which is not that useful. Makes more sense to move this all into here.

--- api/get_changes.js
+++ api/get_changes.js
@@ -1,53 +1,87 @@
-var error = require('debug')('osm-p2p-server:error')
 var xtend = require('xtend')
+var through = require('through2')
+var readonly = require('read-only-stream')
+var once = require('once')
+var to = require('to2')
 
+var errors = require('../lib/errors')
 var toOsmObj = require('../transforms/osm_p2p_to_obj.js')
 
 /**
- * Get the changes in a changeset.
+ * Get the changes in a changeset, as `cb(err, changes)` or as a stream
  * @param  {string}   id  Changeset ID
  * @param  {Object}   osm osm-p2p-db instance
  * @param  {Function} cb  callback(err, array of elements from changeset)
- *                        Elements have the property 'action' which is on of
+ *                        Elements have the property 'action' which is one of
  *                        create|modify|delete
+ * @returns {ReadableStream} Readable object stream of changes
  */
 module.exports = function (osm) {
-  return function getChanges (id, cb) {
-    osm.getChanges(id, function (err, versionIds) {
-      if (err) return cb(err)
-      var docs = []
-      var pending = versionIds.length
-      var errors = []
-
-      if (!versionIds.length) return cb(null, [])
-
-      versionIds.forEach(function (versionId) {
-        osm.log.get(versionId, function (err, doc) {
-          if (err) {
-            errors.push(err)
-            error(err)
-          } else docs.push(doc)
-          if (--pending === 0) {
-            if (errors.length) return cb(new Error(errors.join('\n')))
-            cb(null, docs.map(docmap))
-          }
-        })
-      })
+  return function getChanges (id, opts, cb) {
+    if (typeof opts === 'function') {
+      cb = opts
+      opts = {}
+    }
+    var stream = through.obj(getDoc)
+    osm.get(id, function (err, docs) {
+      if (err) return onError(err)
+      if (!isChangeset(docs)) {
+        return onError(new errors.NotFound('changeset id: ' + id))
+      }
+      var r = osm.changeset.list(id, opts)
+      r.on('error', onError)
+      r.pipe(stream)
     })
+    if (cb) {
+      cb = once(cb)
+      collectObj(stream, cb)
+    } else {
+      return readonly(stream)
+    }
+
+    function getDoc (row, enc, next) {
+      var self = this
+      osm.log.get(row.key, function (err, doc) {
+        if (err) return next(err)
+        var element = xtend(doc.value.v, {
+          id: doc.value.k,
+          version: doc.key,
+          action: getAction(doc)
+        })
+        self.push(toOsmObj.fn(element))
+        next()
+      })
+    }
+
+    function onError (err) {
+      if (cb) cb(err)
+      stream.emit('error', err)
+    }
   }
 }
 
-function docmap (doc) {
-  var element = xtend(doc.value.v, {
-    id: doc.value.k,
-    version: doc.key,
-    action: getAction(doc)
+function isChangeset (docs) {
+  var versions = Object.keys(docs)
+  var result = false
+  versions.forEach(function (version) {
+    if (docs[version].type === 'changeset') result = true
   })
-  return toOsmObj.fn(element)
+  return result
 }
 
 function getAction (doc) {
   if (doc.links.length === 0 && doc.value.d === undefined) return 'create'
   if (doc.links.length > 0 && doc.value.d === undefined) return 'modify'
   if (doc.value.d !== undefined) return 'delete'
+}
+
+function collectObj (stream, cb) {
+  var rows = []
+  stream.on('error', cb)
+  stream.pipe(to.obj(write, end))
+  function write (x, enc, next) {
+    rows.push(x)
+    next()
+  }
+  function end () { cb(null, rows) }
 }
