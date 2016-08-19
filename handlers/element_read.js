e5c59345ca9af79754e926701e21b7d892aadc18
Gregor MacLennan <gmaclennan@digital-democracy.org>
2016-08-18 19:08:11 -0700

    Use query, not params, for requesting all forks

--- handlers/element_read.js
+++ handlers/element_read.js
@@ -1,3 +1,4 @@
+var qs = require('query-string')
 var pump = require('pump')
 var fromArray = require('from2-array')
 
@@ -5,9 +6,10 @@ var wrapResponse = require('../transforms/wrap_response.js')
 var objToXml = require('../transforms/obj_to_xml.js')
 
 module.exports = function (req, res, api, params, next) {
+  var query = qs.parse(qs.extract(req.url))
   api.getElement(params.id, function (err, forks) {
     if (err) return next(err)
-    if (!params.forks) {
+    if (!query.forks) {
       forks = forks.sort(recentFirst)[0]
     }
     res.setHeader('content-type', 'text/xml; charset=utf-8')
@@ -19,6 +21,6 @@ function recentFirst (a, b) {
   if (a.timestamp && b.timestamp) {
     return b.timestamp - a.timestamp
   }
-  // Ensure sorting is consistent between requests
+  // Ensure sorting is stable between requests
   return a.version < b.version ? -1 : 1
 }
