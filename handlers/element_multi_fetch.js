6e437a83ed901c3c98d48f750fd0172c006677f1
Gregor MacLennan <gmaclennan@digital-democracy.org>
2016-08-24 17:14:37 -0700

    Refactor multi_fetch

--- handlers/element_multi_fetch.js
+++ handlers/element_multi_fetch.js
@@ -1,45 +1,47 @@
-var xtend = require('xtend')
+var qs = require('query-string')
+var toOsm = require('obj2osm')
+var fromArray = require('from2-array')
 
 var errors = require('../lib/errors')
-var renderElem = require('../lib/render_elem.js')
-var h = require('../lib/h.js')
 
 module.exports = function (req, res, api, params, next) {
   if (params.type !== params.ktype) {
     return next(new errors.TypeMismatch(params.type, params.ktype))
   }
+  var query = qs.parse(qs.extract(req.url))
   var ids = params.ids.split(',')
   var results = []
   var pending = 1
   var sent = false
-  ids.forEach(function (id, i) {
+  ids.forEach(function (id) {
     pending++
-    api.osm.get(id, function (err, docs) {
+    api.getElement(id, function (err, forks) {
       if (sent) return
       if (err) {
         sent = true
         return next(err)
       }
-      results[i] = []
-      Object.keys(docs).forEach(function (key) {
-        results[i].push(renderElem(xtend(docs[key], {
-          id: id,
-          version: key
-        })))
-      })
+      if (!query.forks) {
+        forks = forks.sort(recentFirst).slice(0, 1)
+      }
+      Array.prototype.push.apply(results, forks)
       if (--pending === 0) done()
     })
   })
   if (--pending === 0) done()
 
   function done () {
-    var docs = []
-    results.forEach(function (group) {
-      group.forEach(function (g) {
-        docs.push(g)
-      })
-    })
+    var r = fromArray.obj(results).on('error', next)
+    var t = toOsm().on('error', next)
     res.setHeader('content-type', 'text/xml; charset=utf-8')
-    res.end(h('osm', docs))
+    r.pipe(t).pipe(res)
   }
+}
+
+function recentFirst (a, b) {
+  if (a.timestamp && b.timestamp) {
+    return b.timestamp - a.timestamp
+  }
+  // Ensure sorting is stable between requests
+  return a.version < b.version ? -1 : 1
 }
