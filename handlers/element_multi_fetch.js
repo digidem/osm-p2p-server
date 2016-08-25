ea8cbbe2486c127b4304dda18077a7db3a6ec0af
Gregor MacLennan <gmaclennan@digital-democracy.org>
2016-08-24 19:45:36 -0700

    Ensure multi_fetch returns elements in order requested

--- handlers/element_multi_fetch.js
+++ handlers/element_multi_fetch.js
@@ -13,7 +13,7 @@ module.exports = function (req, res, api, params, next) {
   var results = []
   var pending = 1
   var sent = false
-  ids.forEach(function (id) {
+  ids.forEach(function (id, i) {
     pending++
     api.getElement(id, function (err, forks) {
       if (sent) return
@@ -24,14 +24,20 @@ module.exports = function (req, res, api, params, next) {
       if (!query.forks) {
         forks = forks.sort(recentFirst).slice(0, 1)
       }
-      Array.prototype.push.apply(results, forks)
+      results[i] = forks
       if (--pending === 0) done()
     })
   })
   if (--pending === 0) done()
 
   function done () {
-    var r = fromArray.obj(results).on('error', next)
+    var flattened = []
+    results.forEach(function (group) {
+      group.forEach(function (g) {
+        flattened.push(g)
+      })
+    })
+    var r = fromArray.obj(flattened).on('error', next)
     var t = toOsm().on('error', next)
     res.setHeader('content-type', 'text/xml; charset=utf-8')
     r.pipe(t).pipe(res)
