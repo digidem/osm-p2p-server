9adb89cb7e9964a7031ace5165043c92634620d6
Gregor MacLennan <gmaclennan@digital-democracy.org>
2016-08-24 20:36:57 -0700

    add non-streaming option for getMap()

--- api/get_map.js
+++ api/get_map.js
@@ -1,3 +1,4 @@
+var collect = require('collect-stream')
 var pumpify = require('pumpify')
 var mapStream = require('through2-map')
 
@@ -5,10 +6,21 @@ var refs2nodes = require('../lib/util').refs2nodes
 var checkRefExist = require('../lib/check_ref_ex.js')
 
 module.exports = function (osm) {
-  return function getMap (bbox, opts) {
+  return function getMap (bbox, opts, cb) {
+    if (typeof opts === 'function') {
+      cb = opts
+      opts = {}
+    }
     var query = [[bbox[1], bbox[3]], [bbox[0], bbox[2]]] // left,bottom,right,top
     var queryStream = osm.queryStream(query, { order: 'type' })
     var queryStreamJson = pumpify.obj(queryStream, checkRefExist(osm), mapStream.obj(refs2nodes))
-    return queryStreamJson
+    if (cb) {
+      collect(queryStreamJson, function (err, elements) {
+        if (err) return cb(err)
+        cb(null, elements)
+      })
+    } else {
+      return queryStreamJson
+    }
   }
 }
