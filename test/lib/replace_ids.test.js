3ad51b8d635ee402653c859f5093424da1342ccb
Gregor MacLennan <gmaclennan@digital-democracy.org>
2016-10-15 07:29:09 -0500

    Changeset upload placeholder ids only unique within type

--- test/lib/replace_ids.test.js
+++ test/lib/replace_ids.test.js
@@ -2,6 +2,8 @@ var test = require('tape')
 
 var replacePlaceholderIds = require('../../lib/replace_ids')
 
+// TODO: Tests for relations
+
 test('replacePlaceholderIds', t => {
   var input = [
     {action: 'create', type: 'node', id: 'A'},
@@ -21,7 +23,7 @@ test('replacePlaceholderIds', t => {
   })
 })
 
-test('replacePlaceholderIds: duplicate ids', t => {
+test('replacePlaceholderIds: duplicate node ids', t => {
   var input = [
     {action: 'create', type: 'node', id: '-123'},
     {action: 'create', type: 'node', id: '-123'}
@@ -29,6 +31,49 @@ test('replacePlaceholderIds: duplicate ids', t => {
   replacePlaceholderIds(input, function (err) {
     t.true(err instanceof Error, 'returns error')
     t.true(/#-123/.test(err.message), 'error references dup id')
+    t.end()
+  })
+})
+
+test('replacePlaceholderIds: duplicate way ids', t => {
+  var input = [
+    {action: 'create', type: 'way', id: '-123'},
+    {action: 'create', type: 'way', id: '-123'}
+  ]
+  replacePlaceholderIds(input, function (err) {
+    t.true(err instanceof Error, 'returns error')
+    t.true(/#-123/.test(err.message), 'error references dup id')
+    t.end()
+  })
+})
+
+test('replacePlaceholderIds: duplicate relation ids', t => {
+  var input = [
+    {action: 'create', type: 'relation', id: '-123'},
+    {action: 'create', type: 'relation', id: '-123'}
+  ]
+  replacePlaceholderIds(input, function (err) {
+    t.true(err instanceof Error, 'returns error')
+    t.true(/#-123/.test(err.message), 'error references dup id')
+    t.end()
+  })
+})
+
+test('replacePlaceholderIds: placeholder ids can be duplicated between types', t => {
+  var input = [
+    {action: 'create', type: 'node', id: 'A'},
+    {action: 'create', type: 'way', id: 'A', nodes: ['A']},
+    {action: 'modify', type: 'way', id: 'B', nodes: ['A', 'X']}
+  ]
+  replacePlaceholderIds(input, function (err, result) {
+    t.error(err)
+    t.notEqual(result[0].id, input[0].id, 'id replaced')
+    t.equal(result[0].old_id, input[0].id, 'old_id prop is placeholder id')
+    t.notEqual(result[1].id, input[1].id, 'id replaced')
+    t.equal(result[1].old_id, input[1].id, 'old_id prop is placeholder id')
+    t.equal(result[1].nodes[0], result[0].id, 'created way ref updated')
+    t.deepEqual(result[2].nodes, [result[0].id, 'X'], 'modified node ref updated')
+    t.equal(result[2].old_id, undefined, 'old_id not set on modified way')
     t.end()
   })
 })
