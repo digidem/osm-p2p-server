798aa38680557422cbd54997c75e134ef07089b9
Stephen Whitmore <sww@eight45.net>
2017-01-10 18:24:24 -0800

    Add unit test for way node deletion fork scenario.

--- test/lib/filter_forked_elements.test.js
+++ test/lib/filter_forked_elements.test.js
@@ -130,6 +130,44 @@ test('forked ways', function (t) {
   t.end()
 })
 
+test('forked ways /w a delete', function (t) {
+  var elements = [
+    {
+      type: 'way',
+      id: '100',
+      version: 'boop',
+      nodes: ['123', '124']
+    },
+    {
+      type: 'way',
+      id: '100',
+      version: 'beep',
+      nodes: ['123']
+    },
+    {
+      type: 'node',
+      id: '123',
+      version: 'foo',
+      lon: 0,
+      lat: 0
+    },
+    {
+      type: 'node',
+      id: '124',
+      version: 'bar',
+      lon: 1,
+      lat: 1
+    }
+  ]
+
+  var expected = elements.slice(1, 3)
+
+  var actual = filterForkedElements(elements)
+
+  t.deepEqual(actual.sort(cmpElement), expected.sort(cmpElement))
+  t.end()
+})
+
 function cmpElement (a, b) {
   if (a.id === b.id) return b.version - a.version
   else return b.id - a.id
