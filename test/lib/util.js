ed19f1a93fb9a29ca8cf47c6ff152d1708a52301
Gregor MacLennan <gmaclennan@digital-democracy.org>
2017-01-14 17:11:29 +0000

    Breaking test: compare function

--- test/lib/util.js
+++ test/lib/util.js
@@ -33,3 +33,81 @@ test('map function', t => {
   t.notEqual(util.refs2nodes(input), input, 'does not mutate input')
   t.end()
 })
+
+test('cmpFork latest first', t => {
+  var input = [{
+    id: 1,
+    timestamp: '2017-01-14T16:43:38.968Z',
+    version: 'b'
+  }, {
+    id: 2,
+    timestamp: '2017-01-14T16:43:36.100Z',
+    version: 'a'
+  }]
+  t.equal(input.sort(util.cmpFork)[0].id, 1)
+  t.end()
+})
+
+test('cmpFork latest last', t => {
+  var input = [{
+    id: 1,
+    timestamp: '2017-01-14T16:43:36.100Z',
+    version: 'b'
+  }, {
+    id: 2,
+    timestamp: '2017-01-14T16:43:38.968Z',
+    version: 'a'
+  }]
+  t.equal(input.sort(util.cmpFork)[0].id, 2)
+  t.end()
+})
+
+test('cmpFork only first has timestamp', t => {
+  var input = [{
+    id: 1,
+    timestamp: '2017-01-14T16:43:36.100Z',
+    version: 'b'
+  }, {
+    id: 2,
+    version: 'a'
+  }]
+  t.equal(input.sort(util.cmpFork)[0].id, 1)
+  t.end()
+})
+
+test('cmpFork only second has timestamp', t => {
+  var input = [{
+    id: 1,
+    version: 'b'
+  }, {
+    id: 2,
+    timestamp: '2017-01-14T16:43:36.100Z',
+    version: 'a'
+  }]
+  t.equal(input.sort(util.cmpFork)[0].id, 2)
+  t.end()
+})
+
+test('cmpFork no timestamps (use version)', t => {
+  var input = [{
+    id: 1,
+    version: 'b'
+  }, {
+    id: 2,
+    version: 'a'
+  }]
+  t.equal(input.sort(util.cmpFork)[0].id, 2)
+  t.end()
+})
+
+test('cmpFork no timestamps (use version)', t => {
+  var input = [{
+    id: 2,
+    version: 'a'
+  }, {
+    id: 1,
+    version: 'b'
+  }]
+  t.equal(input.sort(util.cmpFork)[0].id, 2)
+  t.end()
+})
